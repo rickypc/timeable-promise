1f25dc45d3c10f2bbe51eda16b0be528dcef22e0
Ricky Huang <rickypc@users.noreply.github.com>
2023-04-09 08:58:32 -0700

    Adjust example file.

--- example.js
+++ example.js
@@ -1,10 +1,13 @@
-const { untilSettledOrTimedOut } = require('timeable-promise');
+// eslint-disable-next-line import/no-unresolved
+const { untilSettledOrTimedOut, waitFor } = require('timeable-promise');
 
 (async () => {
   const logs = ['1. Settle Example'];
   let response = await untilSettledOrTimedOut(async (resolve, reject, pending) => {
     // Simulate processing time shorter than timeout.
-    const processed = await new Promise(done => setTimeout(done, 10, true));
+    const processed = await new Promise((done) => {
+      setTimeout(done, 10, true);
+    });
     if (pending()) {
       logs.push(`   => resolving: ${processed}`);
       resolve(processed);
@@ -22,7 +25,9 @@ const { untilSettledOrTimedOut } = require('timeable-promise');
   logs.push('2. Timeout Example');
   response = await untilSettledOrTimedOut(async (resolve, reject, pending) => {
     // Simulate processing time longer than timeout.
-    const processed = await new Promise(done => setTimeout(done, 100, true));
+    const processed = await new Promise((done) => {
+      setTimeout(done, 100, true);
+    });
     if (pending()) {
       logs.push(`   => resolving: ${processed}`);
       resolve(processed);
@@ -36,7 +41,24 @@ const { untilSettledOrTimedOut } = require('timeable-promise');
       return false;
     });
   logs.push(`   => response: ${response}`);
+
+  logs.push('3. Wait Example');
+  // Simulate long process running.
+  let inflight = true;
+  const predicate = () => !inflight;
+  const timeout = 5000;
+  setTimeout(() => {
+    // Simulate long process completion.
+    inflight = false;
+  }, 1000);
+  // eslint-disable-next-line no-console
+  console.time('Wait');
+  await waitFor(predicate, timeout);
+
+  // eslint-disable-next-line no-console
   console.log(...logs);
+  // eslint-disable-next-line no-console
+  console.timeEnd('Wait');
 
   return 'Timeable Promise Examples';
 })();
