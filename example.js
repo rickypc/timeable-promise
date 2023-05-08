d1f5cbb9c952b3356ab2015fcc759914fcde2ae6
Ricky Huang <rickypc@users.noreply.github.com>
2023-05-07 19:43:00 -0700

    Adjust the examples.

--- example.js
+++ example.js
@@ -1,15 +1,61 @@
 const {
+  chunk,
+  concurrent,
+  concurrents,
+  consecutive,
+  consecutives,
   parallel,
   poll,
   sequential,
   sleep,
+  toNumber,
   untilSettledOrTimedOut,
   waitFor,
   // eslint-disable-next-line import/no-unresolved
 } = require('timeable-promise');
 
 (async () => {
-  const logs = ['1. Parallel Example'];
+  const logs = ['1. Chunk Example'];
+  const chunked = chunk([1, 2, 3, 4], 2);
+  logs.push('   => chunked: ', chunked);
+
+  logs.push('2. Concurrent Example');
+  const concurrentSettled = await concurrent(['a', 'b', 'c'], async (value, index) => {
+    // Simulate long process running.
+    await sleep(100);
+    logs.push(`   => concurrent: ${index}-${value}`);
+    return value;
+  });
+  logs.push('   => concurrent settled: ', concurrentSettled);
+
+  logs.push('3. Concurrents Example');
+  const concurrentsSettled = await concurrents([['a', 'b'], ['c']], async (value, index) => {
+    // Simulate long process running.
+    await sleep(100);
+    logs.push(`   => concurrent: ${index}-${value}`);
+    return value;
+  });
+  logs.push('   => concurrents settled: ', concurrentsSettled);
+
+  logs.push('4. Consecutive Example');
+  const consecutiveSettled = await consecutive(['a', 'b', 'c'], async (value, index) => {
+    // Simulate long process running.
+    await sleep(100);
+    logs.push(`   => consecutive: ${index}-${value}`);
+    return value;
+  });
+  logs.push('   => consecutive settled: ', consecutiveSettled);
+
+  logs.push('5. Consecutives Example');
+  const consecutivesSettled = await consecutives([['a', 'b'], ['c']], async (value, index) => {
+    // Simulate long process running.
+    await sleep(100);
+    logs.push(`   => consecutive: ${index}-${value}`);
+    return value;
+  });
+  logs.push('   => consecutives settled: ', consecutivesSettled);
+
+  logs.push('6. Parallel Example');
   const parallelSettled = await parallel(['a', 'b', 'c'], async (value, index) => {
     // Simulate long process running.
     await sleep(100);
@@ -19,13 +65,13 @@ const {
   logs.push('   => parallel settled: ', parallelSettled);
 
   let iter = 1;
-  logs.push('2. Poll Example');
+  logs.push('7. Poll Example');
   const timer = poll(() => {
     logs.push(`   => poll: ${iter += 1}`);
   }, 100);
   setTimeout(() => timer.stop(), 300);
 
-  logs.push('3. Sequential Example');
+  logs.push('8. Sequential Example');
   const sequentialSettled = await sequential(['a', 'b', 'c'], async (value, index) => {
     // Simulate long process running.
     await sleep(100);
@@ -34,13 +80,17 @@ const {
   });
   logs.push('   => sequential settled: ', sequentialSettled);
 
-  logs.push('4. Sleep Example');
+  logs.push('9. Sleep Example');
   const s0 = performance.now();
   await sleep(1000);
   const s1 = performance.now();
   logs.push(`   => slept: ${s1 - s0}ms`);
 
-  logs.push('5. Settle Example');
+  logs.push('10. ToNumber Example');
+  const converted = toNumber('1');
+  logs.push('   => converted: ', converted);
+
+  logs.push('11. Settle Example');
   let response = await untilSettledOrTimedOut(async (resolve, reject, pending) => {
     // Simulate processing time shorter than timeout.
     const processed = await new Promise((done) => {
@@ -60,7 +110,7 @@ const {
     });
   logs.push(`   => response: ${response}`);
 
-  logs.push('6. Timeout Example');
+  logs.push('12. Timeout Example');
   response = await untilSettledOrTimedOut(async (resolve, reject, pending) => {
     // Simulate processing time longer than timeout.
     const processed = await new Promise((done) => {
@@ -80,7 +130,7 @@ const {
     });
   logs.push(`   => response: ${response}`);
 
-  logs.push('7. WaitFor Example');
+  logs.push('13. WaitFor Example');
   // Simulate long process running.
   let inflight = true;
   const predicate = () => !inflight;
